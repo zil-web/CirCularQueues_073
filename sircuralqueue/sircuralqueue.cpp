8245af9c2110521dfc900001befb589ed9ad1f11
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-23 14:49:03 +0700

    Add project files.

---
+++ sircuralqueue/sircuralqueue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
+public:
+	queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter the number: ";
+		cin >> num;
+	}
+
+
+};
