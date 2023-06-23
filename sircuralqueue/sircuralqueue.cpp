518f8d6a0d3bad0c83d2759f4d896671b2e52496
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-23 15:31:57 +0700

    jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- sircuralqueue/sircuralqueue.cpp
+++ sircuralqueue/sircuralqueue.cpp
@@ -80,7 +80,11 @@ public:
 		cout << "\nelements in the queue are..\n";
 
 		// jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array 
-		while (){}
+		while (FRONT_position <= REAR_position){
+			cout << queue_array[FRONT_position] << "  ";
+			FRONT_position++;
+		}
+		cout << endl;
 	}
 	
 
