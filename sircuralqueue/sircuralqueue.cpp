9b56585a5da9d244c2812be10dcf12100fb6ead1
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-23 14:56:17 +0700

    cek apakah antrian kosong

--- sircuralqueue/sircuralqueue.cpp
+++ sircuralqueue/sircuralqueue.cpp
@@ -19,8 +19,15 @@ public:
 
 		// cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-			COUT << "\n Queue overflow\n";
+			cout << "\n Queue overflow\n";
+		return;
 	}
 
+	// cek apakah antrian kosong
+	if (FRONT == -1) {
+		FRONT = 0;
+		REAR = 0;
+	}
+		
 
 };
