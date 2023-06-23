fe774342f9806abed3b59f5e2d028157c6c4a675
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-23 14:59:53 +0700

    jika REAR beada di posisi terakhir maka array, kembali ke awal array

--- sircuralqueue/sircuralqueue.cpp
+++ sircuralqueue/sircuralqueue.cpp
@@ -21,13 +21,23 @@ public:
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
 			cout << "\n Queue overflow\n";
 		return;
+
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// jika REAR beada di posisi terakhir maka array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR = 1;
+		}
+		queue_array[REAR] = num;
+
 	}
 
-	// cek apakah antrian kosong
-	if (FRONT == -1) {
-		FRONT = 0;
-		REAR = 0;
-	}
-		
+	
 
 };
